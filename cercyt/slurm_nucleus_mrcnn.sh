5340ca51831978e787bbec55f0491f5101120a12
Jie Zou <zouj.rpi@gmail.com>
2019-02-28 14:51:03 -0500

    nucleus_mrcnn training is ready

---
+++ cercyt/slurm_nucleus_mrcnn.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --workdir=/slurm_storage/jzou/exp/CerCyt/nucleus_mrcnn/
+#SBATCH --output=/slurm_storage/jzou/exp/CerCyt/nucleus_mrcnn/slurm_%j.out
+#SBATCH --error=/slurm_storage/jzou/exp/CerCyt/nucleus_mrcnn/slurm_%j.error
+#SBATCH --job-name=nucleus_mrcnn
+#SBATCH --gres=gpu:1
+#SBATCH --partition=dgx1
+
+export LD_LIBRARY_PATH=/slurm_storage/public/cuda9.0/lib64
+
+# env
+which python
+python /slurm_storage/jzou/programs/CerCyt/cercyt/nucleus_mrcnn.py\
+    --weights mask_rcnn_coco.h5\
+    --dataset /slurm_storge/jzou/datasets/data-science-bowl-2018/
+    --subset stage1_train
