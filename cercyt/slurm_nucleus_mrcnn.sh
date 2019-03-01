5fbea94b612551065ad1e192099bab1736a2b57e
Jie Zou <zouj.rpi@gmail.com>
2019-03-01 10:35:12 -0500

    Fix slurm sh bug

--- cercyt/slurm_nucleus_mrcnn.sh
+++ cercyt/slurm_nucleus_mrcnn.sh
@@ -11,7 +11,7 @@ export LD_LIBRARY_PATH=/slurm_storage/public/cuda9.0/lib64
 
 # env
 which python
-python /slurm_storage/jzou/programs/CerCyt/cercyt/nucleus_mrcnn.py\
+python /slurm_storage/jzou/programs/CerCyt/cercyt/nucleus_mrcnn.py train\
     --weights /slurm_storage/jzou/programs/CerCyt/models/mask_rcnn_coco.h5\
     --dataset /slurm_storge/jzou/datasets/data-science-bowl-2018\
     --subset stage1_train
