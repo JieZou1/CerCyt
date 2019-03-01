c2d4c4489457debb19c237e24d49b504cffb2dda
Jie Zou <zouj.rpi@gmail.com>
2019-03-01 10:54:43 -0500

    Fix typo in slurm sh

--- cercyt/slurm_nucleus_mrcnn.sh
+++ cercyt/slurm_nucleus_mrcnn.sh
@@ -11,4 +11,4 @@ export LD_LIBRARY_PATH=/slurm_storage/public/cuda9.0/lib64
 
 # env
 which python
-python /slurm_storage/jzou/programs/CerCyt/cercyt/nucleus_mrcnn.py train --weights coco --dataset /slurm_storge/jzou/datasets/data-science-bowl-2018 --subset stage1_train
+python /slurm_storage/jzou/programs/CerCyt/cercyt/nucleus_mrcnn.py train --weights coco --dataset /slurm_storage/jzou/datasets/data-science-bowl-2018 --subset stage1_train
