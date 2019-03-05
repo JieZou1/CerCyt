c8e3aa78c54bf2ad4bad2c1fe6a40132e2aab689
Jie Zou <zouj.rpi@gmail.com>
2019-03-05 12:42:27 -0500

    Fix slurm sh

--- cercyt/nucleus_mrcnn/slurm_nucleus_mrcnn.sh
+++ cercyt/nucleus_mrcnn/slurm_nucleus_mrcnn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --workdir=/slurm_storage/jzou/programs/CerCyt/cercyt/nucleus_mrcnn/
-#SBATCH --output=/slurm_storage/jzou/programs/CerCyt/cercyt/nucleus_mrcnn/slurm_%j.out
-#SBATCH --error=/slurm_storage/jzou/programs/CerCyt/cercyt/nucleus_mrcnn/slurm_%j.error
+#SBATCH --workdir=/slurm_storage/jzou/programs/CerCyt/
+#SBATCH --output=/slurm_storage/jzou/programs/CerCyt/slurm_%j.out
+#SBATCH --error=/slurm_storage/jzou/programs/CerCyt/slurm_%j.error
 #SBATCH --job-name=nucleus_mrcnn
 #SBATCH --gres=gpu:1
 #SBATCH --partition=dgx1
@@ -11,6 +11,7 @@ export LD_LIBRARY_PATH=/slurm_storage/public/cuda9.0/lib64
 # env
 which python
 python /slurm_storage/jzou/programs/CerCyt/cercyt/nucleus_mrcnn/nucleus_mrcnn.py train\
-        --weights coco
-        --dataset /slurm_storage/jzou/datasets/data-science-bowl-2018
+        --dataset /slurm_storage/jzou/datasets/data-science-bowl-2018\
+        --weights /slurm_storage/jzou/programs/CerCyt/models/mask_rcnn_coco.h5\
+        --logs /slurm_storage/jzou/programs/CerCyt/cercyt/nucleus_mrcnn/logs\
         --subset stage1_train
